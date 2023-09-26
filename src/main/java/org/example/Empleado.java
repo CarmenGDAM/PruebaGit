45afd55c8023c9b5f051aa859ae3dd1a62bd2edc
DAM <g@g.es>
2023-09-26 09:16:26 +0200

    Tercer commit, he creado la clase Empleado

---
+++ src/main/java/org/example/Empleado.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Empleado {
+    int id;
+    String nombre;
+
+    public Empleado(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
